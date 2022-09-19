06ef8f316671ab21504357b231bb62fa8518a9c8
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-09-19 16:52:01 +0500

    * qoshdim

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v0b#lu+$2(!jkfk28r&r&wh#jfe!-k#0o6b7kbq*jhi21+4v@=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
